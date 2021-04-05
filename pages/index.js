1bc0c65b157494ad521e693fa0b5ca5acd578c09
Emin Sahin <m.emins@yahoo.com>
2021-04-06 00:03:50 +0200

    added prettier

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,5 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className="text-purple-500 font-bold">Hello Next+Tailwind</div>
-  )
+  return <div className="text-purple-500 font-bold">Hello Next+Tailwind</div>
 }
