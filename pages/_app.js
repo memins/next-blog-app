b72e3320010e4ca8aa2f1bd91c47554497de157e
Emin Sahin <m.emins@yahoo.com>
2021-04-12 15:00:40 +0200

    added auth0 for comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/globals.css'
 import Header from '../components/header'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-700">
-      <Header></Header>
-      <main className="mt-6">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="emin.us.auth0.com"
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-700">
+        <Header></Header>
+        <main className="mt-6">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
